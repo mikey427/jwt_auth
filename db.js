cdd688ee07be779c8e8fc5e77455942443284681
Michael Baez <mrbaez427@gmail.com>
2021-05-24 15:35:53 -0400

    Added bcrypt hashing

--- db.js
+++ db.js
@@ -1,24 +1,25 @@
-const Sequelize = require("sequelize");
-const jwt = require("jsonwebtoken");
+const Sequelize = require('sequelize');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const { STRING } = Sequelize;
 const config = {
-  logging: false,
+  logging: false
 };
 
 if (process.env.LOGGING) {
   delete config.logging;
 }
 const conn = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost/acme_db",
+  process.env.DATABASE_URL || 'postgres://localhost/acme_db',
   config
 );
 
-const User = conn.define("user", {
+const User = conn.define('user', {
   username: STRING,
-  password: STRING,
+  password: STRING
 });
 
-User.byToken = async (token) => {
+User.byToken = async token => {
   try {
     const verifiedToken = jwt.verify(token, process.env.JWT);
     const user = await User.findByPk(verifiedToken.userId);
@@ -26,55 +27,74 @@ User.byToken = async (token) => {
       return user;
     }
 
-    const error = Error("bad credentials");
+    const error = Error('bad credentials');
     error.status = 401;
     throw error;
   } catch (ex) {
-    const error = Error("bad credentials");
+    const error = Error('bad credentials');
     error.status = 401;
     throw error;
   }
 };
 
 User.authenticate = async ({ username, password }) => {
+  // const hashedPassword = await encrypt(password);
+  // console.log(hashedPassword);
   const user = await User.findOne({
     where: {
-      username,
-      password,
-    },
+      username
+    }
   });
-  if (user) {
+  const correctPassword = await bcrypt.compare(password, user.password);
+  console.log(correctPassword);
+  if (correctPassword) {
     const token = jwt.sign({ userId: user.id }, process.env.JWT);
     return token;
   }
 
-  const error = Error("bad credentials");
+  const error = Error('bad credentials');
   error.status = 401;
   throw error;
 };
 
+const saltRounds = 10;
+
+const encrypt = async password => {
+  const pass = await bcrypt.hash(password, saltRounds);
+  return pass;
+};
+
+User.beforeCreate(async (user, options) => {
+  const hashedPassword = await encrypt(user.password);
+  user.password = hashedPassword;
+});
+
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
   const credentials = [
-    { username: "lucy", password: "lucy_pw" },
-    { username: "moe", password: "moe_pw" },
-    { username: "larry", password: "larry_pw" },
+    {
+      username: 'lucy',
+      password: 'lucy_pw'
+    },
+    { username: 'moe', password: 'moe_pw' },
+    { username: 'larry', password: 'larry_pw' }
   ];
+
   const [lucy, moe, larry] = await Promise.all(
-    credentials.map((credential) => User.create(credential))
+    credentials.map(credential => User.create(credential))
   );
   return {
     users: {
       lucy,
       moe,
-      larry,
-    },
+      larry
+    }
   };
 };
 
 module.exports = {
   syncAndSeed,
   models: {
-    User,
-  },
+    User
+  }
 };
